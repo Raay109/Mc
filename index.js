4e36d1e3df92547548cc6fe8f48c4dd3ea7eb644
Raay109 <156700408+Raay109@users.noreply.github.com>
2024-09-15 08:11:45 +0000

    upd

--- index.js
+++ index.js
@@ -1,15 +1,31 @@
-const {Authflow, Titles} = require('prismarine-auth')
-const bedrock = require('bedrock-protocol')
+const bedrock = require('bedrock-protocol');
 
-//bedrock protocoll
-const client = bedrock.createClient({
-    host: 'blockbande.de',
-    port: 19132,
-    username: 'Botet',
+const bot = bedrock.createClient({
+    host: 'blockbande.de',  
+    port: 19132,            
+    username: 'Botet',      
     offline: false
 })
 
-client.on('chat', (packet) => {
-client.write('/p h !Raay2009')
-console.log('yes?')
-})
+bot.on('spawn', (packet) =>{
+
+    console.log('spawned'),
+    bot.queue('text', {
+        type: 'chat',
+        needs_translation: false,
+        source_name: bot.username,
+        xuid: '',  // XUID des Spielers (optional)
+        platform_chat_id: '',  // Plattform-Chat-ID (optional)
+        filtered_message: '',  // Gefilterte Nachricht (optional)
+        message: '/p h !raay2009'  // Nachricht, die gesendet werden soll
+    });
+});
+
+// Fehlerbehandlung und Verbindungsereignisse
+bot.on('error', (err) => {
+    console.error('Error:', err);
+});
+
+bot.on('disconnect', (reason) => {
+    console.log('Disconnected:', reason);
+});
